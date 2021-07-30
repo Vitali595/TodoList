24eafc0ae2c30b7003c411d7bbd4c7046b601956
Vitali595 <vitaliy.kovalchuk.2001@gmail.com>
2021-07-30 11:57:56 +0300

    redux-toolkit thunk part3

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,31 +1,22 @@
-import {tasksReducer} from "../features/TodolistsList/tasks-reducer"
-import {todolistsReducer} from "../features/TodolistsList/todolists-reducer"
-import {combineReducers} from "redux"
-import thunkMiddleware from "redux-thunk"
-import {appReducer} from "./app-reducer"
-import {authReducer} from "../features/Login/auth-reducer"
-import {configureStore} from "@reduxjs/toolkit"
-import {useDispatch} from "react-redux";
+import {combineReducers} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import {appReducer} from '../features/Application'
+import {authReducer} from '../features/Auth'
+import {tasksReducer, todolistsReducer} from '../features/TodolistsList'
+import {configureStore} from '@reduxjs/toolkit'
 
-
-const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
+export const rootReducer = combineReducers({
     app: appReducer,
-    auth: authReducer
+    auth: authReducer,
+    todolists: todolistsReducer,
+    tasks: tasksReducer
 })
-
-export type RootReducerType = typeof rootReducer
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
 })
 
-export type AppRootStateType = ReturnType<RootReducerType>
-
 // @ts-ignore
 window.store = store
 
-type AppDispatchType = typeof store.dispatch
-export const useAppDispatch = () => useDispatch<AppDispatchType>()
